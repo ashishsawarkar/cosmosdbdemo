cc65ca4dbb7fa2eaf219e8f695c540f5bd40a327
Ashish Sawarkar <sawarkar.ashish90@gmail.com>
2020-06-01 00:07:56 +0530

    Second Checkin

---
+++ Models/Services/ICosmosDBService.cs
@@ -0,0 +1,15 @@
+namespace CosmosDBCRUD
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using CosmosDBCRUD.Models;
+
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
